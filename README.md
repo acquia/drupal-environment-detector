d99d78eee04e31144a39c55f1bcef4b106cbe0fb
Dane Powell <git@danepowell.com>
2020-04-22 10:59:42 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+Acquia Environment Detector
+====
+
+This package provides a common static class and methods for detecting the current Acquia Cloud environment.
+
+## Installation and usage
+
+In your project, require the plugin with Composer:
+
+`composer require acquia/environment-detector`
+
+# License
+
+Copyright (C) 2020 Acquia, Inc.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
---
+++ src/AcquiaEnvironmentDetector.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Acquia\EnvironmentDetector;
+
+class AcquiaEnvironmentDetector {
+  /**
+   * Is AH env.
+   */
+  public static function isAhEnv() {
+    return (bool) self::getAhEnv();
+  }
+
+  /**
+   * Check if this is an ACSF env.
+   *
+   * Roughly duplicates the detection logic implemented by the ACSF module.
+   *
+   * @param mixed $ah_group
+   *   The Acquia Hosting site / group name (e.g. my_subscription).
+   * @param mixed $ah_env
+   *   The Acquia Hosting environment name (e.g. 01dev).
+   *
+   * @return bool
+   *   TRUE if this is an ACSF environment, FALSE otherwise.
+   *
+   * @see https://git.drupalcode.org/project/acsf/blob/8.x-2.62/acsf_init/lib/sites/default/acsf.settings.php#L14
+   */
+  public static function isAcsfEnv($ah_group = NULL, $ah_env = NULL) {
+    if (is_null($ah_group)) {
+      $ah_group = self::getAhGroup();
+    }
+
+    if (is_null($ah_env)) {
+      $ah_env = self::getAhEnv();
+    }
+
+    if (empty($ah_group) || empty($ah_env)) {
+      return FALSE;
+    }
+
+    return file_exists("/mnt/files/$ah_group.$ah_env/files-private/sites.json");
+  }
+
+  /**
+   * Is AH prod.
+   */
+  public static function isAhProdEnv() {
+    $ah_env = self::getAhEnv();
+    // ACE prod is 'prod'; ACSF can be '01live', '02live', ...
+    return $ah_env == 'prod' || preg_match('/^\d*live$/', $ah_env);
+  }
+
+  /**
+   * Is AH stage.
+   */
+  public static function isAhStageEnv() {
+    $ah_env = self::getAhEnv();
+    // ACE staging is 'test' or 'stg'; ACSF is '01test', '02test', ...
+    return preg_match('/^\d*test$/', $ah_env) || $ah_env == 'stg';
+  }
+
+  /**
+   * Is AH dev.
+   */
+  public static function isAhDevEnv() {
+    // ACE dev is 'dev', 'dev1', ...; ACSF dev is '01dev', '02dev', ...
+    return (preg_match('/^\d*dev\d*$/', self::getAhEnv()));
+  }
+
+  /**
+   * Is AH ODE.
+   */
+  public static function isAhOdeEnv($ah_env = NULL) {
+    if (is_null($ah_env)) {
+      $ah_env = self::getAhEnv();
+    }
+    // CDEs (formerly 'ODEs') can be 'ode1', 'ode2', ...
+    return (preg_match('/^ode\d*$/', $ah_env));
+  }
+
+  /**
+   * Is AH devcloud.
+   */
+  public static function isAhDevCloud() {
+    return (!empty($_SERVER['HTTP_HOST']) && strstr($_SERVER['HTTP_HOST'], 'devcloud'));
+  }
+
+  /**
+   * Get AH group.
+   */
+  public static function getAhGroup() {
+    return getenv('AH_SITE_GROUP');
+  }
+
+  /**
+   * Get AH env.
+   */
+  public static function getAhEnv() {
+    return getenv('AH_SITE_ENVIRONMENT');
+  }
+
+  /**
+   * Get AH files.
+   */
+  public static function getAhFilesRoot() {
+    return '/mnt/files/' . self::getAhGroup() . '.' . self::getAhEnv();
+  }
+
+  /**
+   * Get ACSF db.
+   *
+   * @return string|null
+   *   ACSF db name.
+   */
+  public static function getAcsfDbName() {
+    return isset($GLOBALS['gardens_site_settings']) && self::isAcsfEnv() ? $GLOBALS['gardens_site_settings']['conf']['acsf_db_name'] : NULL;
+  }
+}
