7bd7903feeff29e37b239e65db8b0f1612a887b8
Dane Powell <git@danepowell.com>
2021-02-19 17:48:44 -0800

    Add settings file path

--- src/FilePaths.php
+++ src/FilePaths.php
@@ -10,6 +10,29 @@ namespace Acquia\DrupalEnvironmentDetector;
 class FilePaths {
 
   /**
+   * Path to primary site settings file with db, memcache, and other info.
+   *
+   * @param string $ah_group
+   *   The Acquia Hosting site group.
+   * @param string $site_name
+   *   The site name (e.g. `default`).
+   *
+   * @return string
+   *   The path to the settings include file.
+   */
+  public static function ahSettingsFile(string $ah_group, string $site_name) {
+    // The default site uses ah_group-settings.inc.
+    if ($site_name === 'default') {
+      $site_name = $ah_group;
+    }
+
+    // Acquia Cloud does not support periods in db names.
+    $site_name = str_replace('.', '_', $site_name);
+
+    return "/var/www/site-php/$ah_group/$site_name-settings.inc";
+  }
+
+  /**
    * Path to sites.json on ACSF.
    *
    * This file contains information about all sites on an ACSF subscription.
@@ -20,6 +43,7 @@ class FilePaths {
    *   The Acquia Hosting environment.
    *
    * @return string
+   *   The path to sites.json.
    */
   public static function acsfSitesJson(string $ah_group, string $ah_env) {
     return "/mnt/files/$ah_group.$ah_env/files-private/sites.json";
